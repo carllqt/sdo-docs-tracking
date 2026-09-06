018d36cdb5bd245af118e689d75bcee706ad180a
carllqt <reycarlmedico@gmail.com>
2026-09-07 00:21:19 +0800

    okay nayan bukas ulit

---
+++ app/Models/DocumentMovement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentMovement extends Model
+{
+    /** @use HasFactory<\Database\Factories\DocumentMovementFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'document_id',
+    ];
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
+}
